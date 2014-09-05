ea25813f3cbd8bc9061280f8930904d7bb5457d1
Myxomatosis <lucianom96@gmail.com>
2014-09-05 09:12:52 -0600

    Assignment #2

--- Python/Homeworks/Assignment #2/Part 1/Census.py
+++ Python/Homeworks/Assignment #2/Part 1/Census.py
@@ -1,7 +1,8 @@
-#	Author: Luciano Mejia
-#	Date: 9/4/14
-#	Assignment #2 Part 1
-#	Calculates the population of the U.S. in  one year.
+'''	Author: Luciano Mejia
+	Date: 9/4/14
+	Assignment #2 Part 1
+	Calculates the population of the U.S. in  one year.
+'''
 
 # Variables
 cur_pop = 318892103
@@ -11,7 +12,7 @@ num_of_births = 31536000/8 # 3,942,000 births/year
 num_of_deaths = 31536000/13 # About 2425846 deaths/year
 num_of_immigrants = 31536000/40 # 788,400 immigrants/year
 
-# Calculating the projected population
-projected_pop = (cur_pop + num_of_births + num_of_immigrants) - num_of_deaths
+# Calculating the projected population: First the population increase (births/immigrants) is added to the current population, then the number of deaths is subtracted from that sum
+projected_pop = (cur_pop + num_of_births + num_of_immigrants) - num_of_deaths  # Uses the previously found number of births/deaths/immigrants
 
-print ("The population will be %.f" % projected_pop)
+print ("The population will be %.f" % projected_pop) # Population of 321,196,657
